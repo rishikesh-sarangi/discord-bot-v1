65841ab372c6c8819dfa3c867dcb3507f0f01dd1
Rishikesh Sarangi <rishikeshsarangi70@gmail.com>
2025-09-23 23:48:41 +0530

    ANTI KUNNING 100% WORKING

--- utils/segragate_kunning.py
+++ utils/segragate_kunning.py
@@ -1,16 +1,5 @@
 def isolate_kunning(ctx):
     author = ctx.author 
-    info = [
-        f"**id:** {author.id}",
-        f"**name (username handle):** {author.name}",
-        f"**discriminator:** {author.discriminator}",   
-        f"**str(author):** {str(author)}",              
-        f"**global_name (Display Name):** {author.global_name}",
-        f"**display_name (guild-aware):** {author.display_name}",
-        f"**nick (nickname, guild only):** {getattr(author, 'nick', None)}",
-        f"**mention:** {author.mention}",
-    ]
 
-    print(info)
+    return True if author.id is '770109102742962176' else False
 
-    return False
