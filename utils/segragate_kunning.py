805d053aae185d8ea27f64fa0aaf8e0dfc5e42d0
Rishikesh Sarangi <rishikeshsarangi70@gmail.com>
2025-09-23 23:51:09 +0530

    small fix

--- utils/segragate_kunning.py
+++ utils/segragate_kunning.py
@@ -1,5 +1,5 @@
 def isolate_kunning(ctx):
     author = ctx.author 
 
-    return True if author.id is '770109102742962176' else False
+    return True if author.id == '770109102742962176' or 770109102742962176 else False
 
